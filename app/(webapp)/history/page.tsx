d391f42ccb3d049713c1205737130e8cedb7465a
AKisliy <lekha.a-kisel@yandex.ru>
2025-05-04 17:59:26 +0300

    Fix layout on profile and history pages

--- app/(webapp)/history/page.tsx
+++ app/(webapp)/history/page.tsx
@@ -1,13 +1,14 @@
+import { Search } from 'lucide-react';
+import { Suspense } from 'react';
+
 import { HistoryEntriesFallback } from '@/components/history/fallback';
 import HistoryEntriesWrapper from '@/components/history/wrapper';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Search } from 'lucide-react';
-import { Suspense } from 'react';
 
 export default async function SearchHistory() {
   return (
-    <Card className="w-full max-w-3xl mx-auto bg-background">
+    <Card className="w-full max-w-3xl mx-auto bg-background mt-10">
       <CardHeader className="flex flex-row items-center space-y-0 pb-4">
         <CardTitle className="text-xl font-semibold">Ваша история</CardTitle>
       </CardHeader>
