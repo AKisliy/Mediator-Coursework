b16fe2bdbe4deb9c1ffebca3bfc36f37666567bc
AKisliy <lekha.a-kisel@yandex.ru>
2025-05-07 11:45:33 +0300

    Fix plans page layout

--- app/(webapp)/plans/page.tsx
+++ app/(webapp)/plans/page.tsx
@@ -5,14 +5,16 @@ export default async function PlansPage() {
   const plans = await getAllPlans();
   const currentUserPlan = await getUserPlan();
   return (
-    <div className="grid grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-4 w-full max-w-4xl p-5 md:mt-20">
-      {plans?.map(plan => (
-        <PlanCard
-          key={plan.id}
-          plan={plan}
-          currentPlanId={currentUserPlan?.id}
-        />
-      ))}
+    <div className="flex flex-grow h-full w-full items-center justify-center">
+      <div className="grid grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-4 w-full h-fit max-w-4xl p-5 md:scale-125">
+        {plans?.map(plan => (
+          <PlanCard
+            key={plan.id}
+            plan={plan}
+            currentPlanId={currentUserPlan?.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
