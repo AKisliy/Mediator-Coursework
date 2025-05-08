5e42e2af4ae5e2a099ca8fccdc7645cf80ba0243
AKisliy <lekha.a-kisel@yandex.ru>
2025-05-08 08:08:06 +0300

    Fix Redis auth error

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,10 @@
 import Redis from 'ioredis';
 
-export const redisConnection = new Redis(process.env.REDIS_URL, {
+export const redisConnection = new Redis({
+  username: 'default',
+  password: process.env.REDIS_PASSWORD,
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
   maxRetriesPerRequest: null,
   enableReadyCheck: false
 });
